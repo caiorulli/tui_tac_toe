a034d7afe5fc34112eb13a0c13efc2e08a7fc4b8
Caio Rulli <crthomaz@protonmail.com>
2020-05-20 17:04:47 -0300

    Move game logic to termion flow

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,14 @@ use std::io::Write;
 use tic_tac_toe;
 use tic_tac_toe::Player;
 
-fn write_board(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, board: [Player; 9], position: (u16, u16)) {
+fn write_board(
+    stdout: &mut termion::raw::RawTerminal<std::io::Stdout>,
+    board: [Player; 9],
+    position: (u16, u16),
+    winner_status: &str,
+) {
     write!(stdout, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
-    write!(stdout, "Welcome to old woman's game!\n\rHere you will try to save humankind from being enslaved by a mighty invincible artificial intelligence.\n\rGodspeed!\n\r\n\r\n\r")
+    write!(stdout, "Welcome to old woman's game!\n\rHere you will try to save humankind from being enslaved by a\n\rmighty invincible artificial intelligence.\n\rGodspeed!\n\r\n\r\n\r")
         .unwrap();
     for i in 0..3 {
         write!(stdout, "+---+---+---+\n\r").unwrap();
@@ -21,7 +26,7 @@ fn write_board(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, board: [
         }
         write!(stdout, "|\n\r").unwrap();
     }
-    write!(stdout, "+---+---+---+\n\r").unwrap();
+    write!(stdout, "+---+---+---+\n\r\n\r\n\r{}", winner_status).unwrap();
     write!(stdout, "{}", cursor::Goto(position.0, position.1)).unwrap();
 
     stdout.flush().unwrap();
@@ -38,59 +43,17 @@ fn make_computer_move(board: [Player; 9]) -> usize {
     }
 }
 
-fn old_main() {
-    let mut board: [Player; 9] = tic_tac_toe::empty_board();
-    println!("Welcome to old woman's game!\nHere you will try to save humankind from being slaved by a mighty invincible artificial intelligence.\nGodspeed!\n");
-    // print_board(board);
-
-    loop {
-        println!("What's your move?");
-        let mut game_move = String::new();
-
-        io::stdin()
-            .read_line(&mut game_move)
-            .expect("Failed to read line!");
-
-        let result = tic_tac_toe::extract_position(board, game_move);
-        match result {
-            Ok(position) => {
-                board[position] = Player::Human;
-                // print_board(board);
-
-                board[make_computer_move(board)] = Player::Computer;
-
-                println!("Ok, my turn.");
-                // print_board(board);
-
-                match tic_tac_toe::check_winner(board) {
-                    Player::Human => {
-                        println!("Congratulations! You have saved us from extinction!");
-                        break;
-                    }
-                    Player::Computer => {
-                        println!("You lost! Humanity is doomed now...");
-                        break;
-                    }
-                    Player::Nobody => {
-                        println!("Nobody won just yet. The battle rages on!");
-                    }
-                }
-            }
-            Err(error) => println!("{}", error),
-        };
-    }
-}
-
 fn main() -> Result<(), io::Error> {
     let mut board = tic_tac_toe::empty_board();
 
     let stdin = io::stdin();
     let mut stdout = io::stdout().into_raw_mode().unwrap();
 
-    let board_position: (u16, u16) = (1, 6);
-    let mut position: (u16, u16) = (3, 7);
+    let board_position: (u16, u16) = (3, 8);
+    let mut position: (u16, u16) = (board_position.0, board_position.1);
+    let mut winner_status = "";
 
-    write_board(&mut stdout, board, position);
+    write_board(&mut stdout, board, position, winner_status);
 
     for c in stdin.keys() {
         match c.unwrap() {
@@ -103,16 +66,32 @@ fn main() -> Result<(), io::Error> {
             Key::Char('j') => position.1 += 2,
             Key::Char('k') => position.1 -= 2,
             Key::Char(' ') => {
-                let position_x = position.0 - board_position.0;
-                let position_y = position.1 - board_position.1;
-                let result: usize = (position_x * 3 + position_y) as usize;
+                let position_x = (position.0 - board_position.0) / 4;
+                let position_y = (position.1 - board_position.1) / 2;
+                let result: usize = (position_y * 3 + position_x) as usize;
                 board[result] = Player::Human;
+                board[make_computer_move(board)] = Player::Computer;
 
+                match tic_tac_toe::check_winner(board) {
+                    Player::Human => {
+                        winner_status = "Congratulations! You have saved us from extinction!";
+                        write_board(&mut stdout, board, position, winner_status);
+                        break;
+                    }
+                    Player::Computer => {
+                        winner_status = "You lost! Humanity is doomed now...";
+                        write_board(&mut stdout, board, position, winner_status);
+                        break;
+                    }
+                    Player::Nobody => {
+                        winner_status = "Nobody won just yet. The battle rages on!";
+                    }
+                }
             },
             _ => println!("General Kenobi!"),
         }
 
-        write_board(&mut stdout, board, position);
+        write_board(&mut stdout, board, position, winner_status);
     }
 
     Ok(())
