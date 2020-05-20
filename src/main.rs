67ea1ef763a202e18efe7a857ef7635fe11a5e5a
Caio Rulli <crthomaz@protonmail.com>
2020-05-20 16:31:39 -0300

    Termion support: cursor is moving!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 use rand::Rng;
-use std::io;
+use termion::clear;
+use termion::cursor;
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Direction, Layout};
-use tui::widgets::{Block, Borders, Widget};
-use tui::Terminal;
+use std::io;
+use std::io::Write;
 
 use tic_tac_toe;
 use tic_tac_toe::Player;
 
-fn print_board(board: [Player; 9]) {
+fn write_board(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, board: [Player; 9], position: (u16, u16)) {
+    write!(stdout, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
+    write!(stdout, "Welcome to old woman's game!\n\rHere you will try to save humankind from being enslaved by a mighty invincible artificial intelligence.\n\rGodspeed!\n\r\n\r\n\r")
+        .unwrap();
     for i in 0..3 {
-        println!("+---+---+---+");
+        write!(stdout, "+---+---+---+\n\r").unwrap();
         for j in 0..3 {
-            print!("| {} ", board[i * 3 + j]);
+            write!(stdout, "| {} ", board[i * 3 + j]).unwrap();
         }
-        println!("|");
+        write!(stdout, "|\n\r").unwrap();
     }
-    println!("+---+---+---+");
+    write!(stdout, "+---+---+---+\n\r").unwrap();
+    write!(stdout, "{}", cursor::Goto(position.0, position.1)).unwrap();
+
+    stdout.flush().unwrap();
 }
 
 fn make_computer_move(board: [Player; 9]) -> usize {
@@ -34,7 +41,7 @@ fn make_computer_move(board: [Player; 9]) -> usize {
 fn old_main() {
     let mut board: [Player; 9] = tic_tac_toe::empty_board();
     println!("Welcome to old woman's game!\nHere you will try to save humankind from being slaved by a mighty invincible artificial intelligence.\nGodspeed!\n");
-    print_board(board);
+    // print_board(board);
 
     loop {
         println!("What's your move?");
@@ -48,12 +55,12 @@ fn old_main() {
         match result {
             Ok(position) => {
                 board[position] = Player::Human;
-                print_board(board);
+                // print_board(board);
 
                 board[make_computer_move(board)] = Player::Computer;
 
                 println!("Ok, my turn.");
-                print_board(board);
+                // print_board(board);
 
                 match tic_tac_toe::check_winner(board) {
                     Player::Human => {
@@ -75,25 +82,38 @@ fn old_main() {
 }
 
 fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.draw(|mut f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10),
-                ]
-                .as_ref(),
-            )
-            .split(f.size());
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
-        let block = Block::default().title("Block 2").borders(Borders::ALL);
-        f.render_widget(block, chunks[1]);
-    })
+    let mut board = tic_tac_toe::empty_board();
+
+    let stdin = io::stdin();
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+
+    let board_position: (u16, u16) = (1, 6);
+    let mut position: (u16, u16) = (3, 7);
+
+    write_board(&mut stdout, board, position);
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => {
+                write!(stdout, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
+                break;
+            },
+            Key::Char('l') => position.0 += 4,
+            Key::Char('h') => position.0 -= 4,
+            Key::Char('j') => position.1 += 2,
+            Key::Char('k') => position.1 -= 2,
+            Key::Char(' ') => {
+                let position_x = position.0 - board_position.0;
+                let position_y = position.1 - board_position.1;
+                let result: usize = (position_x * 3 + position_y) as usize;
+                board[result] = Player::Human;
+
+            },
+            _ => println!("General Kenobi!"),
+        }
+
+        write_board(&mut stdout, board, position);
+    }
+
+    Ok(())
 }
